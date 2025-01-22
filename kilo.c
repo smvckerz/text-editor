807ec70ef4721a0b2a47a44f1a853550d512e072
eduardo <edmunoz1030@gmail.com>
2025-01-21 19:27:11 -0800

    added a few more changes

--- kilo.c
+++ kilo.c
@@ -5,12 +5,21 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
-struct termios orig_termios;
+struct editorConfig
+{
+  int screenrows;
+  int screencols;
+
+  struct termios orig_termios;
+};
+
+struct editorConfig E; /* Editor state */
 
 /*** terminal ***/
 void die(const char *s)
@@ -24,7 +33,7 @@ void die(const char *s)
 
 void disableRawMode()
 {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
   {
     die("tcsetattr");
   }
@@ -32,13 +41,13 @@ void disableRawMode()
 
 void enableRawMode()
 {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
   {
     die("tcgetattr");
   }
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_cflag |= (CS8);
   raw.c_oflag &= ~(OPOST);
@@ -68,11 +77,56 @@ char editorReadKey()
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) 
+{
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+  {
+    return -1;
+  }
+  printf("\r\n");
+  char c;
+
+  while (read(STDIN_FILENO, &c, 1) == 1)
+  {
+    if (iscntrl(c))
+    {
+      printf("%d\r\n", c);
+    } 
+    else 
+    {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  }
+  editorReadKey();
+  return -1;
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+  struct winsize ws;
+
+  if(1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+  {
+    if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+    {
+      return -1;
+    }
+    return getCursorPosition(rows, cols);
+  }
+  else
+  {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+
+    return 0;
+  }
+}
+
 /*** output ***/
 void editorDrawRows()
 {
   int y;
-  for (y= 0; y < 24; y++)
+  for (y= 0; y < E.screenrows; y++)
   {
     write(STDIN_FILENO, "~\r\n", 3);
   }
@@ -103,9 +157,18 @@ void editorProcessKeyPress()
 }
 
 /*** init ***/
+void initEditor()
+{
+  if(getWindowSize(&E.screenrows, &E.screencols) == -1)
+  {
+    die("getWindowSize");
+  }
+}
+
 int main()
 {
   enableRawMode();
+  initEditor();
 
   while(1)
   {
@@ -152,4 +215,13 @@ This is useful in canonical mode
 termios can be read into struct by tcgetattr
 
 ECHO is a bit flag
+*/
+
+
+/*
+Last part left off at:
+
+We’re going to have to parse this response. But first, let’s read it into a buffer.
+We’ll keep reading characters until we get to the R character.
+
 */
